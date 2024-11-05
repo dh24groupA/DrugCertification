2896172fb7f113fe29a8536e727b96d9edabfb36
yuchi8313 <yuu08142003@icloud.com>
2024-11-05 21:13:21 +0900

    Initial Commit

---
+++ DrugCertification/DrugCertificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrugCertificationApp.swift
+//  DrugCertification
+//
+//  Created by 植松優羽 on 2024/11/05.
+//
+
+import SwiftUI
+
+@main
+struct DrugCertificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrugCertificationTests/DrugCertificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrugCertificationTests.swift
+//  DrugCertificationTests
+//
+//  Created by 植松優羽 on 2024/11/05.
+//
+
+import Testing
+@testable import DrugCertification
+
+struct DrugCertificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
